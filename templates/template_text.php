65bd351780d998d30eb6b08618c4f7a2c81721d2
mrdragonraaar <ade@mrdragonraaar.com>
2013-10-01 13:54:34 +0100

    Corrected match for opening body tag.

--- templates/template_text.php
+++ templates/template_text.php
@@ -5,7 +5,7 @@
  * (c)2013 mrdragonraaar.com
  */
 $html = $mobipocket->html(); 
-$html = preg_replace('/^.+<body>/s', '', $html);
+$html = preg_replace('/^.+?<body>/s', '', $html);
 $html = preg_replace('/<\/body>.+?$/s', '', $html);
 echo $html;
 ?>
